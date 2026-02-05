e0093566246af84bfb93fd82d6995f8aa5721cd5
Serge KOUAME <sergekouame@192.168.1.2>
2026-02-05 03:55:10 +0000

    modules tâche - terminé

--- backend/src/modules/tasks/tasks.module.ts
+++ backend/src/modules/tasks/tasks.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { User } from '../users/entities/user.entity';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Task, User]),
+    NotificationsModule,
+  ],
+  controllers: [TasksController],
   providers: [TasksService],
-  controllers: [TasksController]
+  exports: [TasksService],
 })
 export class TasksModule {}
